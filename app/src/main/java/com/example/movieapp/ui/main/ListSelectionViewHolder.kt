c9f9821acdb9fbf44a02f15d48b0ae75a03d563a
102300 <berkayen1@gmail.com>
2021-10-01 13:41:06 +0300

    first commit

---
+++ app/src/main/java/com/example/movieapp/ui/main/ListSelectionViewHolder.kt
@@ -0,0 +1,7 @@
+package com.example.movieapp.ui.main
+
+import androidx.recyclerview.widget.RecyclerView
+import com.example.movieapp.databinding.ListSelectionViewHolderBinding
+
+class ListSelectionViewHolder ( val binding: ListSelectionViewHolderBinding) :
+    RecyclerView.ViewHolder(binding.root){}
